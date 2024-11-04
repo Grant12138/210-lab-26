5d6a20844206a886ce11267e5e136387ad338b35
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-11-03 20:02:56 -0800

    Final push

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 */
 #include <iostream>
 #include <fstream>
-#include <chrono>
 #include <vector>
 #include <algorithm>
 #include <set>
@@ -19,8 +18,12 @@ const int ROWS = 4, COLS = 3;
 const int W1 = 10;
 const int RUNS = 15; // Run the experiment 15 times
 
+void print_id(string const& lab_desc);
+
 int main()
 {
+    print_id("Lab 26: Data Structures Olympics");
+
     // Use long long to prevent overflow
     long long results[RUNS][ROWS][COLS] {};
     long long subtotals[ROWS][COLS] {};
@@ -204,6 +207,16 @@ int main()
         }
         cout << "\n";
     }
+    cout << "\n";
 
     return 0;
+}
+
+void print_id(string const& lab_desc)
+{
+    cout << "\nCOMSC210 | Grant Luo | " << lab_desc << "\n";
+    cout << "Editor: CLion\n";
+    cout << "Compiler: Apple clang version 16.0.0\n";
+    cout << "File: " << __FILE__ << "\n";
+    cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
 }
